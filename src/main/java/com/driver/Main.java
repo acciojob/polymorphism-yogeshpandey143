c1b092c6530e857de588754ff71b03a7ea47b4c4
Yogesh <bc21143@smsvaranasi.in>
2022-12-10 16:00:47 +0530

    my coomit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,34 @@ package com.driver;
 
 public class Main {
 
+
+        public static class Product{
+        public Product() {
+
+        }
+
+        public int product(int x ,int y)
+        {
+            return (x*y);
+        }
+        public int product(int x,int y,int z)
+        {
+            return (x*y*z);
+        }
+        public double product(double x,double y)
+        {
+                return (x*y);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p=new Product();
+
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(2.4,3.2));
+
+    }
+
 }
